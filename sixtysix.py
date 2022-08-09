31751ec52073aa4933e7919b801474a3799bba67
celepa <pawel.celejewski@gmail.com>
2022-08-09 02:33:54 +0200

    Add Player and Game classes. Dealing cards.

--- sixtysix.py
+++ sixtysix.py
@@ -1,13 +1,13 @@
+from random import shuffle
+
 class Card:
-    suits = ["spades", "diamonds", "hearts", "clubs"]
-    values = [None, None, "2", "3", "4", "5", "6", "7", "8", "9", "10", "jack", "queen", "king", "ace"]
+    suits = ["S", "D", "H", "C"]
+    values = [None, None, "2", "3", "4", "5", "6", "7", "8", "9", "T", "J", "Q", "K", "A"]
     def __init__(self, value, suit):
         self.value = value
         self.suit = suit
     def __repr__(self):
-        return self.values[self.value] + " of " + self.suits[self.suit]
-
-from random import shuffle
+        return self.values[self.value] + self.suits[self.suit]
 
 class Deck:
     def __init__(self, stripped_deck_card = 2):
@@ -21,9 +21,47 @@ class Deck:
             return
         return self.cards.pop()
 
-deck = Deck(9)
-cards_number = 0
-while len(deck.cards):
-    cards_number+=1
-    print(str(cards_number) + " " + str(deck.rm_card()))
+class Player:
+    def __init__(self, name):
+        self.name = name
+        self.cards = []
+
+class Game:
+    dealer = 0
+    players = []
+    def __init__(self):
+        self.deck = Deck(9)
+        for i in range(0,4):
+            self.players.append(Player(i))
+
+    def deal_cards(self):
+        while self.deck.cards:
+            self.players[0].cards.append(self.deck.rm_card())
+            self.players[1].cards.append(self.deck.rm_card())
+            self.players[2].cards.append(self.deck.rm_card())
+            self.players[3].cards.append(self.deck.rm_card())
+    def play_game(self):
+        self.deal_cards()
+        print("P1: ", end = '')
+        for i in range(0, 6):
+            print(self.players[0].cards[i], end = ' ')
+        print("\nP2: ", end = '')
+        for i in range(0, 6):
+            print(self.players[1].cards[i], end = ' ')
+        print("\nP3: ", end = '')
+        for i in range(0, 6):
+            print(self.players[2].cards[i], end = ' ')
+        print("\nP4: ", end = '')
+        for i in range(0, 6):
+            print(self.players[3].cards[i], end = ' ')
+        
+
+game = Game()
+game.play_game()
+
+# deck = Deck(9)
+# cards_number = 0
+# while len(deck.cards):
+#     cards_number+=1
+#     print(str(cards_number) + " " + str(deck.rm_card()))
 
